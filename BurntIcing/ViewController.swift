0d18f519fad33e8886b88580b3f5fb1d059da4c6
Patrick Smith <pgwsmith@gmail.com>
2015-02-14 18:31:10 +1100

    Initial Commit

---
+++ BurntIcing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BurntIcing
+//
+//  Created by Patrick Smith on 14/02/2015.
+//  Copyright (c) 2015 Burnt Caramel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
